6fb8cc5b99f52c347d9b3972ead8e793676a7225
Poornanand Verma <poornanand95@gmail.com>
2021-05-10 16:25:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Inputkeys from './components/inputkeys';
+
+function App() {
+  return (
+    <div className='App'>
+      <Inputkeys />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/inputkeys.js
@@ -0,0 +1,196 @@
+import React, { Component } from 'react';
+import './inputkeys.css';
+
+export class Inputkeys extends Component {
+  render() {
+    return (
+      <div name='calc' className='container'>
+        <h1>Calculator</h1>
+        <input
+          placeholder='enter your numbers'
+          className='input-area'
+          id='text'
+          name='txt'
+          readOnly=''
+          onChange={this.handleChange}
+        ></input>
+        <div id='answer' className='solution'></div>
+        <form className='wrapper'>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '1';
+            }}
+          >
+            1
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '2';
+            }}
+          >
+            2
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '3';
+            }}
+          >
+            3
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '+';
+            }}
+          >
+            +
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '4';
+            }}
+          >
+            4
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '5';
+            }}
+          >
+            5
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '6';
+            }}
+          >
+            6
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '-';
+            }}
+          >
+            -
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '7';
+            }}
+          >
+            7
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '8';
+            }}
+          >
+            8
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '9';
+            }}
+          >
+            9
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '*';
+            }}
+          >
+            x
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              let str = document.getElementById('text').value;
+              document.getElementById('text').value = str.slice(
+                0,
+                str.length - 1
+              );
+            }}
+          >
+            back
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '0';
+            }}
+          >
+            0
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value = '';
+              document.getElementById('answer').innerText = '';
+            }}
+          >
+            clear
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '/';
+            }}
+          >
+            /
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '(';
+            }}
+          >
+            (
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += ')';
+            }}
+          >
+            )
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              try {
+                var calculatedAns = eval(document.getElementById('text').value);
+                console.log(calculatedAns);
+                document.getElementById('answer').innerHTML = calculatedAns;
+              } catch (err) {
+                document.getElementById('answer').innerHTML = err.message;
+              }
+            }}
+          >
+            ans
+          </div>
+          <div
+            className='btn'
+            onClick={() => {
+              document.getElementById('text').value += '%';
+            }}
+          >
+            %
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Inputkeys;
